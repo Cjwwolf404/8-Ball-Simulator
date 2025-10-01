84aa04d9bedcb298abd3fafd6b19b48a1bcda8ed
Cjwwolf404 <157659337+Cjwwolf404@users.noreply.github.com>
2025-10-01 17:17:20 -0500

    added an optimism slider

--- 8-Ball-Simulator/Assets/Scripts/Magic8Ball.cs
+++ 8-Ball-Simulator/Assets/Scripts/Magic8Ball.cs
@@ -3,47 +3,74 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using TMPro;
+using UnityEngine.UI;
 
 public class Magic8Ball : MonoBehaviour
 {
     public TextMeshProUGUI answerText;
+
+    public Slider optimismSlider;
+    private float optimismLevel = 0.5f;
+
     private bool isRunning = false;
     public float fadeDuration;
-    private string[] answers =
+
+    private string[] positiveAnswers =
     {
         "Yes",
-        "No",
         "Possibly",
-        "Ask again later",
         "Outlook good",
-        "Outlook not good",
         "Your future looks bright",
-        "Your future looks grim",
         "There's a high possibility",
-        "Very doubtful",
     };
 
-    // Start is called before the first frame update
-    void Start()
+    private string[] negativeAnswers =
     {
-
-    }
+        "No",
+        "Ask again later",
+        "Outlook not good",
+        "Your future looks grim",
+        "Very doubtful",
+    };
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && !isRunning)
         {
-            int index = Random.Range(0, answers.Length);
-            answerText.text = answers[index];
-            StartCoroutine(AnswerAppear());
+            ChooseAnswer();
         }
+    }
+
+    public void ChooseAnswer()
+    {
+        float rand = Random.value;
+        string answer;
+
+        if (rand < (1 - optimismLevel))
+        {
+            answer = negativeAnswers[Random.Range(0, negativeAnswers.Length)];
+        }
+        else
+        {
+            answer = positiveAnswers[Random.Range(0, positiveAnswers.Length)];
+        }
+
+        answerText.text = answer;
+        StartCoroutine(AnswerAppear());
+    }
+
+    public void ChangeOptimismLevel()
+    {
+        optimismLevel = optimismSlider.value;
     }
 
     IEnumerator AnswerAppear()
     {
         isRunning = true;
+
         float fadeTimer = 0f;
+
         while (fadeTimer < fadeDuration)
         {
             float alpha = fadeTimer / fadeDuration;
@@ -55,6 +82,7 @@ public class Magic8Ball : MonoBehaviour
         yield return new WaitForSeconds(1f);
 
         fadeTimer = 0;
+
         while (fadeTimer < fadeDuration)
         {
             float alpha = 1 - (fadeTimer / fadeDuration);
